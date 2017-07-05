5ed7a00b77f82ce8ff12d4e50cfba27265822960
Guil Sa <guilsa001@gmail.com>
2017-07-05 14:48:38 -0700

    Basic dice roll

---
+++ schema/main.js
@@ -0,0 +1,29 @@
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList
+} = require('graphql');
+
+const roll = () => Math.floor(6 * Math.random()) + 1;
+
+const queryType = new GraphQLObjectType({
+  name: 'RootQuery',
+  fields: {
+    hello: {
+      type: GraphQLString,
+      resolve: () => 'world',
+    },
+    diceRoll: {
+      type: new GraphQLList(GraphQLInt),
+      resolve: () => [roll(), roll()]
+    }
+  }
+});
+
+const mySchema = new GraphQLSchema({
+  query: queryType
+});
+
+module.exports = mySchema;
